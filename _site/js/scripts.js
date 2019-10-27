d22f91eb853dd1a25b47379a1ec601c656fe80d8
liamgillies <lwgillies@gmail.com>
2019-10-26 22:22:53 -0400

    added jquery scripts

---
+++ _site/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $("#box1").mouseleave(function() {
+    $("#box1").scrollTop(0);
+    });
+});
+$(document).ready(function() {
+    $("#box2").mouseleave(function() {
+    $("#box2").scrollTop(0);
+    });
+});
+$(document).ready(function() {
+    $("#box3").mouseleave(function() {
+    $("#box3").scrollTop(0);
+    });
+});
